20c5ace1daff36fd94e2f8058c32faba8eebe738
Sooyoung Yoon <ayoooo240@gmail.com>
2025-08-26 22:06:06 +0900

    chore: react 외 제거

--- packages/vue-query-devtools/eslint.config.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-
-// @ts-expect-error
-import pluginVue from 'eslint-plugin-vue'
-import rootConfig from './root.eslint.config.js'
-
-export default [...rootConfig, ...pluginVue.configs['flat/base']]
